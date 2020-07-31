7b23c5eb93fcdda4ff30ab181e803a7e00add75f
aizemski <aleksanderizemski@gmail.com>
2020-07-31 09:27:13 +0200

    Updates

---
+++ precache-manifest.7f2470c7e49fdd3c79bc940b0e3dd539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dca61b921cf82cc81f74be56fa049cd",
+    "url": "/foreign_exchange_rates_api/index.html"
+  },
+  {
+    "revision": "ef30d95727ae419c7d48",
+    "url": "/foreign_exchange_rates_api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "45b20d61934c6ddc91dc",
+    "url": "/foreign_exchange_rates_api/static/js/2.735c7d41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foreign_exchange_rates_api/static/js/2.735c7d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef30d95727ae419c7d48",
+    "url": "/foreign_exchange_rates_api/static/js/main.fe0ae806.chunk.js"
+  },
+  {
+    "revision": "94b293092bdaba9412c7",
+    "url": "/foreign_exchange_rates_api/static/js/runtime-main.cb60cd67.js"
+  }
+]);
