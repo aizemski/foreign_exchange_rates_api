5f65d96b819f169ee3d8dace0072f02b34facafc
aizemski <aleksanderizemski@gmail.com>
2020-07-31 11:40:57 +0200

    Selected base and symbol based on queryString

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react';
 import getSymbolFromCurrency from 'currency-symbol-map';
 import './App.css';
 import { getSymbols, getRateWithBase } from './context/fetchData';
+import { Currencies } from './components/Currencies';
 function App() {
     const [forex, setForex] = useState('');
     const [symbols, setSymbols] = useState([]);
+    const [selectedBase, setSelectedBase] = useState('');
+    const [selectedSymbol, setSelectedSymbol] = useState('');
+    const queryString = window.location.search.slice(1);
 
     useEffect(() => {
         const fetchSymbols = async () => {
             if (symbols) setSymbols(await getSymbols());
+            if (queryString) {
+                const requestData = queryString.split('&');
+                setSelectedBase(requestData[0].split('=')[1]);
+                setSelectedSymbol(requestData[1].split('=')[1]);
+            }
         };
 
         fetchSymbols();
@@ -16,21 +25,37 @@ function App() {
     return (
         <div className="container">
             {symbols ? (
-                <form>
-                    <label for="currencies-base">Choose a base:</label>
-                    <select id="currencies-base" name="base">
-                        {symbols.map((symbol) => {
-                            return <option value={symbol}>{symbol}</option>;
-                        })}
-                    </select>
-                    <label for="currencies-symbols">Choose a symbol:</label>
-                    <select id="currencies-symbols" name="symbols">
-                        <option value="All">All</option>
-                        {symbols.map((symbol) => {
-                            return <option value={symbol}>{symbol}</option>;
-                        })}
-                    </select>
-                </form>
+                <>
+                    <form>
+                        <label for="currencies-base">Choose a base:</label>
+                        <select id="currencies-base" name="base">
+                            {symbols.map((symbol) => {
+                                if (symbol == selectedBase)
+                                    return (
+                                        <option value={symbol} selected>
+                                            {symbol}
+                                        </option>
+                                    );
+                                return <option value={symbol}>{symbol}</option>;
+                            })}
+                        </select>
+                        <label for="currencies-symbols">Choose a symbol:</label>
+                        <select id="currencies-symbols" name="symbols">
+                            <option value="All">All</option>
+                            {symbols.map((symbol) => {
+                                if (symbol == selectedSymbol)
+                                    return (
+                                        <option value={symbol} selected>
+                                            {symbol}
+                                        </option>
+                                    );
+                                return <option value={symbol}>{symbol}</option>;
+                            })}
+                        </select>
+                        <input type="submit"></input>
+                    </form>
+                    <Currencies />
+                </>
             ) : (
                 <p>Symbols haven't loaded</p>
             )}
