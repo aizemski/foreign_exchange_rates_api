fcfe1e24c682c13500abec688c256df4281ff092
aizemski <aleksanderizemski@gmail.com>
2020-07-31 11:12:30 +0200

    Displaying currencies, prepared function to fetch specyfic pairs/list of currencies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import getSymbolFromCurrency from 'currency-symbol-map';
 import './App.css';
-import getSymbols from './context/fetchData';
+import { getSymbols, getRateWithBase } from './context/fetchData';
 function App() {
     const [forex, setForex] = useState('');
     const [symbols, setSymbols] = useState([]);
+
     useEffect(() => {
         const fetchSymbols = async () => {
             if (symbols) setSymbols(await getSymbols());
         };
+
         fetchSymbols();
     }, []);
     return (
         <div className="container">
-            <form></form>
+            {symbols ? (
+                <form>
+                    <label for="currencies-base">Choose a base:</label>
+                    <select id="currencies-base" name="base">
+                        {symbols.map((symbol) => {
+                            return <option value={symbol}>{symbol}</option>;
+                        })}
+                    </select>
+                    <label for="currencies-symbols">Choose a symbol:</label>
+                    <select id="currencies-symbols" name="symbols">
+                        <option value="All">All</option>
+                        {symbols.map((symbol) => {
+                            return <option value={symbol}>{symbol}</option>;
+                        })}
+                    </select>
+                </form>
+            ) : (
+                <p>Symbols haven't loaded</p>
+            )}
         </div>
     );
 }
--- src/context/fetchData/index.js
+++ src/context/fetchData/index.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-const getSymbols = async () => {
-    const fetchData = await getData('');
-    var symbols = [];
+export const getSymbols = async () => {
+    const fetchData = await getData();
+    var symbols = ['EUR'];
     for (var symbol in fetchData.rates) {
         symbols.push(symbol);
     }
     return symbols;
 };
 
+export const getRateWithBase = async (base) => {
+    const fetchData = await getData(base);
+    console.log(fetchData);
+};
+
 const getData = async (params) => {
+    console.log(params);
     return await axios
-        .get('https://api.ratesapi.io/api/latest')
+        .get('https://api.ratesapi.io/api/latest', { params })
         .then((response) => {
             return response.data;
         });
 };
-
-export default getSymbols;
