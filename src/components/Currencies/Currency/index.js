c00c49e6b454349f7c7c7748938bad8b2d18e972
aizemski <aleksanderizemski@gmail.com>
2020-08-04 18:56:44 +0200

    Added round to 6 decimal places

--- src/components/Currencies/Currency/index.js
+++ src/components/Currencies/Currency/index.js
@@ -3,7 +3,7 @@ import getSymbolFromCurrency from 'currency-symbol-map';
 export const Currency = (props) => {
     return (
         <tr className="currency-container">
-            <td>{props.rate}</td>
+            <td>{props.rate.toFixed(6)}</td>
             <td>{props.symbol + ' ' + getSymbolFromCurrency(props.symbol)}</td>
         </tr>
     );
